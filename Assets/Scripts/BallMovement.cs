2c0512f69df16a6ddbe158c254a5eb29bbb49b4a
Blaze <116586998+BlazeThundra@users.noreply.github.com>
2026-01-12 10:37:16 -0600

    Initialize Rigidbody2D and update movement logic
    Fix ball movement logic and initialize Rigidbody2D.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+ Rigidbody2D rb;
+
+ void Awake()
+ {
+  rb = GetComponent<Rigidbody2D>();
+ }
+ 
  void Start()
  {
         
@@ -9,11 +16,9 @@ public class BallMovement : MonoBehaviour
 
  void Update()
  {
-        
- }
-
- void OnMouseDown()
- {
-  
+  if(GetKeyDown(KeyCode.Mouse0)
+  {
+   rb.linearvelocity += new Vector3(0,5,0);
+  }  
  }
 }
