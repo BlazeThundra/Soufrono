b579702b5aa9e593e4afb0de043f9e369742de9c
Blaze <116586998+BlazeThundra@users.noreply.github.com>
2026-01-14 15:51:42 -0600

    setup movement and walls

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchManager : MonoBehaviour
+{
+ [SerializeField] GameObject player;
+ [SerializeField] Rigidbody2D rb;
+
+ public Vector2 startPos;
+ public Vector2 endPos;
+ public Vector2 currentPos;
+
+ [SerializeField] float forceMultiplier;
+
+ public void OnTouchPosition(InputAction.CallbackContext ctx)
+ {
+  currentPos = ctx.ReadValue<Vector2>();
+ }
+
+ public void OnTouchPress(InputAction.CallbackContext ctx)
+ {
+  if (ctx.started)
+  {
+   startPos = currentPos;
+  }
+
+  else if (ctx.canceled)
+  {
+   endPos = currentPos;
+   ApplyForce();
+  }
+ }
+
+ public void ApplyForce()
+ {
+  Vector2 dirVector = endPos - startPos;
+  Vector2 normalizedDir = dirVector.normalized;
+  float distance = Vector2.Distance(startPos, endPos);
+
+  rb.AddForce(normalizedDir * (distance * forceMultiplier), ForceMode2D.Impulse);
+  Debug.Log("Apply force");
+ }
+}
