d5796fcf403b9ce2679be6b99db635b601c97e59
Blaze <116586998+BlazeThundra@users.noreply.github.com>
2026-01-16 10:22:52 -0600

    finish

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,16 +1,14 @@
+using UnityEditor.Search;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ public bool scored = false;
+ public int group;
+ 
+ void Awake()
+ {
+//   GameObject obstacleParent = ;
+  transform.parent = null;
+ }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] Transform obstaclesParent;
+    [SerializeField] GameObject ball;
     public int score;
 
     void Awake()
@@ -14,5 +16,15 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
      scoreText.text = score.ToString();
+
+     foreach(Transform obstacle in obstaclesParent)
+     {
+      if(ball.transform.position.y > obstacle.transform.position.y && obstacle.GetComponent<Obstacle>().scored == false)
+      {
+       obstacle.GetComponent<Obstacle>().scored = true;
+       obstacle.GetComponent<SpriteRenderer>().color = Color.red;
+       score ++;
+      }
+     }
     }
 }
