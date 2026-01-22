95c9b20b555d457ce491e0a6d89d3c2e0991cb3c
Blaze <116586998+BlazeThundra@users.noreply.github.com>
2026-01-22 15:56:17 -0600

    Refactor highscore management in SaveManager
    Refactor highscore management to use separate keys for each highscore. Update SaveData and LoadData methods to handle multiple highscores.

--- Assets/SaveManager.cs
+++ Assets/SaveManager.cs
@@ -4,15 +4,21 @@ using UnityEngine.InputSystem;
 
 public class SaveManager : MonoBehaviour
 {
- public int highscore1;
- public int highscore2;
- public int highscore3;
- public int highscore4;
- public int highscore5;
+ [SerializeField] ScoreManager scoreManager;
+
+ public int highscore1 = 0;
+ public int highscore2 = 0;
+ public int highscore3 = 0;
+ public int highscore4 = 0;
+ public int highscore5 = 0;
  
  [SerializeField] TextMeshProUGUI highscoreText;
- const string highscoreKey = "highscore";
-
+ const string highscore1Key = "highscore1";
+ const string highscore2Key = "highscore2";
+ const string highscore3Key = "highscore3";
+ const string highscore4Key = "highscore4";
+ const string highscore5Key = "highscore5";
+  
  void Start()
  {
   LoadData();
@@ -20,7 +26,7 @@ public class SaveManager : MonoBehaviour
 
  void Update()
  {
-  if (Keyboard.current.rKey.isPressed)
+  if (Keyboard.current.rKey.wasPressedThisFrame)
   {
    ResetScore();
   }
@@ -33,28 +39,65 @@ public class SaveManager : MonoBehaviour
 
  void SaveData()
  {
-  PlayerPrefs.SetInt(highscoreKey, GetComponent<ScoreManager>().score);
+  if(scoreManager.score > PlayerPrefs.GetInt(highscore1Key))
+  {
+   PlayerPrefs.SetInt(highscore5Key, PlayerPrefs.GetInt(highscore4Key));
+   PlayerPrefs.SetInt(highscore4Key, PlayerPrefs.GetInt(highscore3Key));
+   PlayerPrefs.SetInt(highscore3Key, PlayerPrefs.GetInt(highscore2Key));
+   PlayerPrefs.SetInt(highscore2Key, PlayerPrefs.GetInt(highscore1Key));
+   PlayerPrefs.SetInt(highscore1Key, scoreManager.score);
+  }
+  else if(scoreManager.score > PlayerPrefs.GetInt(highscore2Key))
+  {
+   PlayerPrefs.SetInt(highscore5Key, PlayerPrefs.GetInt(highscore4Key));
+   PlayerPrefs.SetInt(highscore4Key, PlayerPrefs.GetInt(highscore3Key));
+   PlayerPrefs.SetInt(highscore3Key, PlayerPrefs.GetInt(highscore2Key));
+   PlayerPrefs.SetInt(highscore2Key, scoreManager.score);
+  }
+  else if(scoreManager.score > PlayerPrefs.GetInt(highscore3Key))
+  {
+   PlayerPrefs.SetInt(highscore5Key, PlayerPrefs.GetInt(highscore4Key));
+   PlayerPrefs.SetInt(highscore4Key, PlayerPrefs.GetInt(highscore3Key));
+   PlayerPrefs.SetInt(highscore3Key, scoreManager.score);
+  }
+  else if(scoreManager.score > PlayerPrefs.GetInt(highscore4Key))
+  {
+   PlayerPrefs.SetInt(highscore5Key, PlayerPrefs.GetInt(highscore4Key));
+   PlayerPrefs.SetInt(highscore4Key, scoreManager.score);
+  }
+  else if(scoreManager.score > PlayerPrefs.GetInt(highscore5Key))
+  {
+   PlayerPrefs.SetInt(highscore5Key, scoreManager.score);
+  }
  }
 
  void LoadData()
  {
-  highscore = PlayerPrefs.GetInt(highscoreKey, 0);
-  GetComponent<ScoreManager>().score = 0;
+  highscore1 = PlayerPrefs.GetInt(highscore1Key, 0);
+  highscore2 = PlayerPrefs.GetInt(highscore2Key, 0);
+  highscore3 = PlayerPrefs.GetInt(highscore3Key, 0);
+  highscore4 = PlayerPrefs.GetInt(highscore4Key, 0);
+  highscore5 = PlayerPrefs.GetInt(highscore5Key, 0);
+  scoreManager.score = 0;
   UpdateUI();
  }
 
  void ResetScore()
  {
-  GetComponent<ScoreManager>().score = 0;
-  PlayerPrefs.SetInt(highscoreKey, 0);
+  scoreManager.score = 0;
+  PlayerPrefs.SetInt(highscore1Key, 0);
+  PlayerPrefs.SetInt(highscore2Key, 0);
+  PlayerPrefs.SetInt(highscore3Key, 0);
+  PlayerPrefs.SetInt(highscore4Key, 0);
+  PlayerPrefs.SetInt(highscore5Key, 0);
   UpdateUI();
  }
 
  void UpdateUI()
  {
-  if(highscoreText != null)
-  {
-   highscoreText.text = highscore1.ToString();
-  }
+  // if(highscoreText != null)
+  // {
+  //  highscoreText.text = highscore1.ToString();
+  // }
  }
 }
